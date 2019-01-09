77b12dcdc07947b26f1aad9395b01fd2b8e65e99
dhinojosac <diego@manglar.cl>
2019-01-09 08:31:58 -0300

    Se renueva la libreria

--- ush/ush.go
+++ ush/ush.go
@@ -6,6 +6,7 @@ import (
 
 type User struct {
 	name        string
+	name2       string
 	surname1    string
 	surname2    string
 	age         int
@@ -19,9 +20,38 @@ func CreateUser(n, s1, s2 string) *User {
 	nu.name = n
 	nu.surname1 = s1
 	nu.surname2 = s2
+	nu.entryDate = time.Now()
 	return nu
 }
 
-func (u *User) getName() string {
+func (u *User) SetDescription(d string) {
+	u.description = d
+}
+
+func (u *User) GetDescription() string {
+	return u.description
+}
+
+func (u *User) SetName(n string) {
+	u.name = n
+}
+
+func (u *User) GetName() string {
 	return u.name
+}
+
+func (u *User) GetFullName() string {
+	return u.name + " " + u.name2 + " " + u.surname1 + " " + u.surname2
+}
+
+func (u *User) SetAge(a int) {
+	u.age = a
+}
+
+func (u *User) GetAge() int {
+	return u.age
+}
+
+func (u *User) GetAgeStr() string {
+	return u.entryDate.Format("2006-01-02 15:04:05")
 }
